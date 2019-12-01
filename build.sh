b5b917a90ecbf0940d4593db171af37d67e5ca36
hurd <hurd@Server.hsd1.nh.comcast.net>
2019-12-01 17:58:40 -0500

    Tweaked build script for passing compiler related flags.

--- build.sh
+++ build.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+CC="CC=g++"
+CFG=""
+
+# Get variables passed
+for var in "$@"
+do
+    if [ ${var:0:10} == "CC=clang++" ]
+    then
+        CC="$var"
+    elif [ ${var:0:6} == "CC=g++" ]
+    then
+        CC="$var"
+    elif [ $var == "CFG=Debug" ]
+    then
+        CFG="$var"
+    elif [ $var == "CFG=Release" ]
+    then
+        CFG="$var"
+    else
+        echo "Unrecognized command $var"
+        exit -1
+    fi
+done
+
+# echo "CC=$CC"
+# echo "CFG=$CFG"
+
 pushd src > /dev/null
 
-time make
+time make $CC $CFG
 
 popd > /dev/null
